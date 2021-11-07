bcb73e2b5fd3aa83c188e5e844c28aff76b3ae5d
otinu <65097619+otinu@users.noreply.github.com>
2021-11-07 11:47:34 +0900

    [add]Emotion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 //  import { CssModules } from "./CssModules";
-import { StyledComponents } from "./StyledComponents";
+import { Emotion } from "./Emotion";
 
 //  ReactDOM.render(<CssModules />, document.getElementById("root"));
-ReactDOM.render(<StyledComponents />, document.getElementById("root"));
+//  ReactDOM.render(<StyledComponents />, document.getElementById("root"));
+ReactDOM.render(<Emotion />, document.getElementById("root"));
